c45272718e70622f1a3ae1ddf8006bb536f89654
Matthew Kaufer <matthew@kaufer.org>
2015-11-11 19:53:27 +0000

    Shading & lighting, oh my

--- js/script.js
+++ js/script.js
@@ -22,12 +22,33 @@ function init() {
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog( 0xffffff, 1, 10000 );
 
+	var lights = [];
+	var lightIntensity = 0.3
+	var lightColor = 0xbbbbbb
+	lights[0] = new THREE.PointLight( lightColor, lightIntensity, 0 );
+	lights[1] = new THREE.PointLight( lightColor, lightIntensity, 0 );
+	lights[2] = new THREE.PointLight( lightColor, lightIntensity, 0 );
+
+	lights[0].position.set( 0, 200, 0 );
+	lights[1].position.set( 100, 200, 100 );
+	lights[2].position.set( -100, -200, -100 );
+
+    for(var i = 0; i < lights.length; i++){
+        scene.add(lights[i])
+    }
+
 
     var cubeSize = 10
     var cubeDetail = 5
     var cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize, cubeDetail, cubeDetail, cubeDetail)
-    var cubeMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    var cubeMaterial = new THREE.MeshPhongMaterial({
+        color: 0x2ecc71,
+        emissive: 0x27ae60,
+        side: THREE.DoubleSide,
+        shading: THREE.FlatShading
+    })
     var cube = new THREE.Mesh(cubeGeo, cubeMaterial)
+    cube.geometry.verticesNeedUpdate = true
     
     scene.add(cube)
 
